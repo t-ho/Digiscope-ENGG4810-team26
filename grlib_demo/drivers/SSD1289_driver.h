557aa1d5b7ce27bfd7b40de230dbe115b46a2147
Ryan Fitzsimon <ryan.fitzsimon@uq.net.au>
2016-04-27 16:55:30 +1000

    Add grlib driver and demo

---
+++ grlib_demo/drivers/SSD1289_driver.h
@@ -0,0 +1,16 @@
+/*
+ * SSD1289_driver.h
+ *
+ *  Created on: 26 Apr 2016
+ *      Author: Ryan
+ */
+
+#ifndef SSD1289_DRIVER_H_
+#define SSD1289_DRIVER_H_
+
+extern void SSD1289_Init(void);
+extern void Pant(uint16_t color);
+
+extern const tDisplay g_sKentec320x240x16_SSD2119;
+
+#endif /* SSD1289_DRIVER_H_ */
